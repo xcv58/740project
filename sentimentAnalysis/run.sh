84de4d28be83d401b72bdfbb7b0f536aa9de1609
xcv58 <chenyihonglove@gmail.com>
2013-12-11 17:12:57 -0500

    add org.apache.commons.lang* to local

--- sentimentAnalysis/run.sh
+++ sentimentAnalysis/run.sh
@@ -1,6 +1,6 @@
 rm -r CalculateSentiment
 rm a.jar
-javac -classpath $HADOOP_INSTALL/share/hadoop/common/hadoop-common-2.2.0.jar:$HADOOP_INSTALL/share/hadoop/mapreduce/hadoop-mapreduce-client-core-2.2.0.jar:$HADOOP_INSTALL/share/hadoop/common/lib/commons-cli-1.2.jar:/Users/xcv58/Downloads/eclipse/plugins/org.apache.commons.lang_2.6.0.v201205030909.jar -d . CalculateSentiment.java WordCount.java
+javac -classpath $HADOOP_INSTALL/share/hadoop/common/hadoop-common-2.2.0.jar:$HADOOP_INSTALL/share/hadoop/mapreduce/hadoop-mapreduce-client-core-2.2.0.jar:$HADOOP_INSTALL/share/hadoop/common/lib/commons-cli-1.2.jar:./org.apache.commons.lang_2.6.0.v201205030909.jar -d . CalculateSentiment.java WordCount.java
 jar -cvf a.jar  CalculateSentiment
 hadoop fs -rm -r output
 hadoop fs -put -f CalculateSentiment.obj CalculateSentiment.obj
